1e459ea84c266933f7af572cdcab53558d78f589
lskjames <jamesleesukey@gmail.com>
2018-05-03 14:13:10 +0000

    updated on WW18p4

--- myapi.js
+++ myapi.js
@@ -11,27 +11,33 @@ app.use(express['static'](__dirname ));
 
 // Express route for incoming requests for a customer name
 app.get('/machine/:id/:money', function(req, res, next) {
-	  //res.sendfile('test.html', {output: req.params.id});
+	 // res.sendfile('test.html', {output: req.params.id});
 	  //res.status(200).send(inputs[req.params.id]);
 	console.log(req.params.id)
 	console.log(req.params.money)
-	mqttClient.publish('payment', 'req.params.money')
+	mqttClient.publish(req.params.id, req.params.money)
+	mqttClient.publish('payment', req.params.money)
 });
+
+//app.get('/', function(req, res, next) {
+	  //res.sendfile('test.html', {output: req.params.id});
+	  //res.status(200).send(inputs[req.params.id]);
+//});
 
 // Express route for any other unrecognised incoming requests
-app.get('*', function(req, res) {
-	  res.status(404).send('Unrecognised API call');
-});
+//app.get('*', function(req, res) {
+//	  res.status(404).send('Unrecognised API call');
+//});
 
 // Express route to handle error
-app.use(function(err, req, res, next) {
-	  if (req.xhr) {
-		      res.status(500).send('Oops, Something went wrong!');
-		    } else {
-			        next(err);
-			      }
-});
+//app.use(function(err, req, res, next) {
+//	  if (req.xhr) {
+//		      res.status(500).send('Oops, Something went wrong!');
+//		    } else {
+//			        next(err);
+//			      }
+//});
 
-app.listen(3000);
-console.log('App Server running at port 3000');
+app.listen(80);
+console.log('App Server running at port 80');
 
